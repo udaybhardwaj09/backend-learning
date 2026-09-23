f25ebc0999a4f27c511c9321a3e8d03b31ab47c9
udaybhardwaj09 <udaybhardwaj.2006@gmail.com>
2026-09-23 11:48:17 +0530

    mind-grenade

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 // console.log(names);
 
 const data = require('./names');
-console.log(data);
+require('./mind-grenade');
+// console.log(data);
 // sayHi('susan');
 // sayHi(names.john);
 // sayHi(names.peter);
---
+++ mind-grenade.js
@@ -0,0 +1,8 @@
+const num1 = 10;
+const num2 = 15;
+
+function addValue(){
+    console.log(`${num1 + num2}`);
+}
+
+addValue();
